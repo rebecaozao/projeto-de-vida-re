b8f364eb8e904834e08d46b716cf7e4ea28c29d4
rebecaozao <132940584+rebecaozao@users.noreply.github.com>
2025-06-06 11:15:19 -0300

    Add files via upload

--- js-projeto-de-vida-codigo-base/main.js
+++ js-projeto-de-vida-codigo-base/main.js
@@ -15,7 +15,12 @@ for(let j=0;j <botoes.length;j++){
 }
 
 const contadores = document.querySelectorAll (".contador");
-const tempoObjetivo1 = new Date ("")
-const tempoObjetivo2 = new Date ("")
-const tempoObjetivo3 = new Date ("")
-const tempoObjetivo4 = new Date ("")
+let tempoAtual = new Date ();
+const tempoObjetivo1 = new Date ("2025-12-31T00:00:00") - tempoAtual;
+contadores [0].textContent = tempoObjetivo1
+const tempoObjetivo2 = new Date ("2032-12-31T00:00:00") - tempoAtual;
+contadores [1].textContent = tempoObjetivo2
+const tempoObjetivo3 = new Date ("2034-12-31T00:00:00") - tempoAtual;
+contadores [2].textContent = tempoObjetivo3
+const tempoObjetivo4 = new Date ("2025-12-31T00:00:00") - tempoAtual;
+contadores [3].textContent = tempoObjetivo4
